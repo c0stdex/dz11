2b2b19e31e6206019a98b8497971dae17fdeef3e
c0stdex <123808524+c0stdex@users.noreply.github.com>
2024-07-08 20:31:14 +0300

    Initial commit

---
+++ contacts_api/app/main.py
@@ -0,0 +1,56 @@
+# app/main.py
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from . import crud, models, schemas
+from .database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.post("/contacts/", response_model=schemas.Contact)
+def create_contact(contact: schemas.ContactCreate, db: Session = Depends(get_db)):
+    return crud.create_contact(db=db, contact=contact)
+
+@app.get("/contacts/", response_model=list[schemas.Contact])
+def read_contacts(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    contacts = crud.get_contacts(db, skip=skip, limit=limit)
+    return contacts
+
+@app.get("/contacts/{contact_id}", response_model=schemas.Contact)
+def read_contact(contact_id: int, db: Session = Depends(get_db)):
+    db_contact = crud.get_contact(db, contact_id=contact_id)
+    if db_contact is None:
+        raise HTTPException(status_code=404, detail="Contact not found")
+    return db_contact
+
+@app.put("/contacts/{contact_id}", response_model=schemas.Contact)
+def update_contact(contact_id: int, contact: schemas.ContactCreate, db: Session = Depends(get_db)):
+    db_contact = crud.update_contact(db, contact_id, contact)
+    if db_contact is None:
+        raise HTTPException(status_code=404, detail="Contact not found")
+    return db_contact
+
+@app.delete("/contacts/{contact_id}", response_model=schemas.Contact)
+def delete_contact(contact_id: int, db: Session = Depends(get_db)):
+    db_contact = crud.delete_contact(db, contact_id)
+    if db_contact is None:
+        raise HTTPException(status_code=404, detail="Contact not found")
+    return db_contact
+
+@app.get("/contacts/search/", response_model=list[schemas.Contact])
+def search_contacts(query: str, db: Session = Depends(get_db)):
+    contacts = crud.search_contacts(db, query)
+    return contacts
+
+@app.get("/contacts/upcoming_birthdays/", response_model=list[schemas.Contact])
+def upcoming_birthdays(db: Session = Depends(get_db)):
+    contacts = crud.get_upcoming_birthdays(db)
+    return contacts
---
+++ schemas.py
@@ -0,0 +1,21 @@
+# app/schemas.py
+from pydantic import BaseModel, EmailStr
+from datetime import date
+from typing import Optional
+
+class ContactBase(BaseModel):
+    first_name: str
+    last_name: str
+    email: EmailStr
+    phone_number: str
+    birthday: date
+    additional_info: Optional[str] = None
+
+class ContactCreate(ContactBase):
+    pass
+
+class Contact(ContactBase):
+    id: int
+
+    class Config:
+        orm_mode = True
